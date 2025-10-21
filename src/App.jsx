d16b53bca8e80dd2ac6a136e7cf37ff1f6235a5a
Mehak garg <mehakgarg0070@gmail.com>
2025-10-21 22:31:30 +0530

    Update Navbar import to include file extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar.jsx'
 import { FaEdit } from "react-icons/fa";
 import { AiFillDelete } from "react-icons/ai";
 import { v4 as uuidv4 } from 'uuid';
